e2500c09e1d4beba342306a71387e47d39b53986
JinHoooooou <jinho4744@naver.com>
2020-10-21 18:21:56 +0900

    first commit

---
+++ src/main/java/net/alarm/AlarmApplication.java
@@ -0,0 +1,14 @@
+package net.alarm;
+
+import java.net.URISyntaxException;
+import net.kakao.message.SendToMe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class AlarmApplication {
+
+  public static void main(String[] args) {
+
+  }
+
+}
---
+++ src/test/java/net/alarm/AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package net.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
