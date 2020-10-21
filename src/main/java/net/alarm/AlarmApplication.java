4332b02eb3b900d20b2ab37c25d42c6dcd813d3f
JinHoooooou <jinho4744@naver.com>
2020-10-21 18:34:52 +0900

    add main method

--- src/main/java/net/alarm/AlarmApplication.java
+++ src/main/java/net/alarm/AlarmApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AlarmApplication {
 
   public static void main(String[] args) {
-
+    new SendToMe().sendMessageToMe("테스트입니다. 잘 되나요?");
   }
 
 }
